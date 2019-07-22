b25760fa0a679dc5d357041cc5eef8855187112f
Armando Baños Pascual <armbp1972@gmail.com>
2019-07-22 18:51:50 +0200

    fix in nomenclature of add-wizard

--- src/app/components/add-wizard/add-wizard.component.ts
+++ src/app/components/add-wizard/add-wizard.component.ts
@@ -9,20 +9,20 @@ import { RoutingStep } from '../../infrastructure/model/routing-steps.model';
 })
 export class AddWizardComponent implements OnInit{
 
-  public _steps : string[];
+  public steps : string[];
   public selectedStep: number = 0;
 
   constructor(private router: Router, private route: ActivatedRoute) {
   }
 
   ngOnInit() {
-    this._steps = this.route.snapshot.routeConfig.children.map(child => { return child.path });
-    let step = this._steps[0];
+    this.steps = this.route.snapshot.routeConfig.children.map(child => { return child.path });
+    let step = this.steps[0];
     this.router.navigate([step], { relativeTo: this.route });
   }
 
   selectionChanged(event: any) {
     this.selectedStep = event.selectedIndex;
-    this.router.navigate([this._steps[this.selectedStep]],{relativeTo:this.route});
+    this.router.navigate([this.steps[this.selectedStep]],{relativeTo:this.route});
   }
 }
