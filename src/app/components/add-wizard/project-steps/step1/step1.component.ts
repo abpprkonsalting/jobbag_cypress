d0c000074ee7bfd709a364e9ae0735f7321fe286
Armando Baños Pascual <armbp1972@gmail.com>
2019-07-23 19:34:29 +0200

    starting add-project step 1

--- src/app/components/add-wizard/project-steps/step1/step1.component.ts
+++ src/app/components/add-wizard/project-steps/step1/step1.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { professions } from '../../../../app-constants';
+import { Subscriber } from '../../../custom-stepper/custom-stepper.component';
 
 @Component({
   selector: 'step1',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Step1Component implements OnInit {
 
-  constructor() { }
+  professions: any[];
+
+  constructor(protected messageSubscriber: Subscriber<Partial<any>>) {}
 
   ngOnInit() {
+    this.professions = professions;
+  }
+
+  onCardClick(){
+    this.messageSubscriber.next({value:"next"});
   }
 
 }
