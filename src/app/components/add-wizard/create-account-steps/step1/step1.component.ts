1f73de31e535763cb899d0208c57af44ed58ac4a
Armando Baños Pascual <armbp1972@gmail.com>
2019-08-02 19:19:17 +0200

    finished create-account/step1

--- src/app/components/add-wizard/create-account-steps/step1/step1.component.ts
+++ src/app/components/add-wizard/create-account-steps/step1/step1.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import { DragStepperMessagesHandle } from '../../../drag-stepper/drag-stepper.component';
+import {WebStorageService} from '../../../../services/webstorage.service';
+import { User } from 'src/app/infrastructure/model/user.model';
 
 @Component({
   selector: 'step1',
@@ -8,20 +10,54 @@ import { DragStepperMessagesHandle } from '../../../drag-stepper/drag-stepper.co
   styleUrls: ['./step1.component.less']
 })
 export class Step1Component implements OnInit {
-  firstFormGroup: FormGroup;
+  formGroup: FormGroup;
+  user: User;
 
-  constructor(private _formBuilder: FormBuilder, protected stepperMessagesHandle: DragStepperMessagesHandle<Partial<any>>) { }
+  constructor(private _formBuilder: FormBuilder, protected stepperMessagesHandle: DragStepperMessagesHandle<Partial<any>>, private webstorageService: WebStorageService) { }
 
   ngOnInit() {
-    this.firstFormGroup = this._formBuilder.group({
-      firstCtrl: ['', Validators.required], secondCtrl: ['', Validators.required]
+    this.formGroup = this._formBuilder.group({
+      email: ['', Validators.required], password: ['', Validators.required]
     });
+    this.formGroup.statusChanges.subscribe(status => this.stepperMessagesHandle.next({value:status}));
+    this.stepperMessagesHandle.next({value:'INVALID'});
+
+    this.webstorageService.getUser().subscribe(
+      next => {
+        this.user = next;
+        this.formGroup.controls.email.setValue(this.user.username);
+        this.formGroup.controls.password.setValue(this.user.password);
+        if ( this.user.roles.length > 0 ) { // Esto es que hay un user logged en la aplicación, por lo tanto hay que deslogearlo antes de crear uno nuevo.
+          this.webstorageService.logout().subscribe(next => this.user = next);
+        }
+      },
+      error => {
+        console.log(error.message);
+        this.user = new User();
+      });
+
+      this.stepperMessagesHandle.subscribe(message =>
+        {
+        let messageType = typeof (message.value);
+        if ( messageType == 'string') {
+          switch (message.value) {
+            case "stepperReceivedChangeOrder":
+              this.user.username = this.formGroup.controls.email.value;
+              this.user.password = this.formGroup.controls.password.value;
+              this.webstorageService.setUser(this.user);
+              Object.keys(this.formGroup.controls).forEach((key) => {
+                if (!this.formGroup.get(key).valid) this.formGroup.get(key).markAsTouched();
+              });
+              break;
+            default:
+              break;
+          }
+        }
+      });
   }
 
   onclick($event){
-    if (this.firstFormGroup.status == 'VALID') {
-          this.stepperMessagesHandle.next({value:"next"});
-    }
+    this.stepperMessagesHandle.next({value:"next"});
   }
 
 }
